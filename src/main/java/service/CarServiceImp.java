7305718c16105846a1031690815f9728f11976f2
Denis Akhmetov <jira.gremory@gmail.com>
2024-12-10 03:08:46 +0300

    Убрал лишние импорты

--- src/main/java/service/CarServiceImp.java
+++ src/main/java/service/CarServiceImp.java
@@ -3,8 +3,6 @@ package service;
 import model.Car;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,13 +1,12 @@
 package web.controller;
 
 import model.Car;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import service.CarService;
-import service.CarServiceImp;
+
 
 import java.util.ArrayList;
 import java.util.List;
